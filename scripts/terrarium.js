20f7313ea8bd111ac5b34f619e0f8ea8e0e406ca
Vlad Maniac <vladmaniac@P4509.(none)>
2012-10-30 13:58:12 +0200

    Finish terrarium by adding the actions, bugs and grid in which the bugs move

--- scripts/terrarium.js
+++ scripts/terrarium.js
@@ -107,5 +107,55 @@ testGrid.each(function(point, value) {
   print(point.x, ",", point.y, ": ", value);
 });
 
+// Setup some movement stuff for the bug
+// This is going to assume you have the current position of the insect :)
+// XXX: Tip will use the Point.prototype.add method
+var directions = {
+  "n": new Point(0, 1),
+  "ne": new Point(-1, 1),
+  "nw": new Point(1, 1),
+  "e": new Point(-1, 0),
+  "w": new Point(1,0),
+  "s": new Point(0,-1),
+  "se": new Point(-1, -1),
+  "sw": new Point(1, -1)
+}
+
+// Test some bug directions
+var directionPoint = new Point(5, 5);
+console.log(directions["n"].add(directionPoint));
+
+// Make a stupid bug
+// XXX: The stupid bug will always go south, for the light
+function StupidBug() {};
+
+StupidBug.prototype.act = function(surroundings) {
+  return {type: "move", direction: "s"};
+};
+
+// Create the terrarium object finally
+var wall = {};
+
+function Terrarium(plan) {
+  var grid = new Grid(plan[0].length, plan.length);
+  for (var y = 0; y < plan.length; y++) {
+    var line = plan[y];
+    for (var x = 0; x < line.length; x++) {
+      grid.setValueAt(new Point(x, y),
+                      elementFromCharacter(line.charAt(x)));
+    }
+  }
+  this.grid = grid;
+}
+
+function elementFromCharacter(character) {
+  if (character == " ")
+    return undefined;
+  else if (character == "#")
+    return wall;
+  else if (character == "o")
+    return new StupidBug();
+}
+
 
 
